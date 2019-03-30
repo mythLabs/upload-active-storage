46bdb20b2b6248442f7adcfcc423fce35a94960c
mythlabs <amitbisht744@gmail.com>
2019-03-31 02:41:25 +0530

    Added one_to_many image attachment and handled single delete and multiple delete scenarios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_144930) do
+ActiveRecord::Schema.define(version: 2019_03_30_202220) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 2019_03_30_144930) do
   end
 
   create_table "animals", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "companies", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
